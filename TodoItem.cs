bf5fc6901f4ca68ef44a71bd2c673ab757f0eb83
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-04-19 13:43:18 +0200

    Added base Attributes and Constructor

--- TodoItem.cs
+++ TodoItem.cs
@@ -2,9 +2,20 @@ using System;
 
 namespace EisenhowerMain {
 public class TodoItem
+{
+    private string Title { get; set; }
+    private DateTime Deadline { get; set; }
+    private bool IsDone { get; set; }
+
+    public TodoItem(string title, DateTime deadline)
     {
-
-
+        this.Title = title;
+        this.Deadline = deadline;
+        this.IsDone = false;
     }
+    
+    
+
+}
 
 }
