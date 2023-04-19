16e9efa59570ce0117b86cae21897aa1c4da2d17
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-04-19 21:23:11 +0200

    Added Attributes and Constructor

--- TodoQuarter.cs
+++ TodoQuarter.cs
@@ -5,7 +5,14 @@ using System.Text;
 namespace EisenhowerMain { 
 
     public class TodoQuarter {
+        private List<TodoItem> TodoItems { get; set; }
 
+        public TodoQuarter()
+        {
+            this.TodoItems = new List<TodoItem>();
+        }
+
+        
     }
 
 }
