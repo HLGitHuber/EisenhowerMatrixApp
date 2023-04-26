dac8c10df74b56c03fe3825ea7007f79eefb2c8e
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-04-26 16:15:33 +0200

    added Attribute

--- TodoMatrix.cs
+++ TodoMatrix.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 
 namespace EisenhowerMain
 {
     public class TodoMatrix {
+        private Dictionary<string, TodoQuarter> TodoQuarters { get; set; }
 
         
     }
