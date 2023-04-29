2e6631b03e0cb4c0e1dbaf0c5ba39e9772864b12
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-04-29 10:01:51 +0200

    base for reading with csvhelper

--- TodoMatrix.cs
+++ TodoMatrix.cs
@@ -1,6 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Text;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
 
 namespace EisenhowerMain
 {
@@ -37,6 +45,40 @@ namespace EisenhowerMain
             if (!isImportant && isUrgent) TodoQuarters["NU"].AddItem(title, deadline);
             if (!isImportant && !isUrgent) TodoQuarters["NN"].AddItem(title, deadline);
         }
+        //TODO working reading && writing csv files method
+        public void AddItemsFromFile(string fileName)
+        {
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Encoding = Encoding.UTF8,
+                Delimiter = ","
+            };
+            using (var fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var reader = new StreamReader(fs, Encoding.UTF8))
+                using (var csv = new CsvReader(reader, configuration))
+                {
+                    var records = csv.GetRecords<CsvItem>();
+
+                    foreach (var item in records)
+                    {
+                       //do something with it
+                    }
+                }
+            }
+        }
+    }
+
+    public class CsvItem
+    {
+        [Name("title")]
+        public string Title { get; set; }
+            
+        [Name("day-month")]
+        public string DayMonth { get; set; }
+        
+        [Name("is_important")]
+        public string IsImportant { get; set; }
     }
 
 }
