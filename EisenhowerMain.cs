44cd69b5f31b9e49ce66c3a7ac6b25d5619cdd7a
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-05-06 14:55:19 +0200

    clean code

--- EisenhowerMain.cs
+++ EisenhowerMain.cs
@@ -13,66 +13,57 @@ namespace EisenhowerMain
             var matrix = new TodoMatrix();
             var display = new Display();
             
-            Logic();
-
-            void Logic()
+            bool exit = false;
+            while (!exit)
             {
-                bool exit = false;
-                while (!exit)
-                {
-                    Console.WriteLine(matrix.ToString());
-                    display.ShowMenu();
-                    
-                    string menuInput = Console.ReadLine();
-                    TodoQuarter quarter;
-                    string title;
-                    DateTime deadline;
-                    bool isImportant;
+                Console.WriteLine(matrix.ToString());
+                display.ShowMenu();
+                
+                string menuInput = Console.ReadLine();
+                TodoQuarter quarter;
 
-                    switch (menuInput)
-                    {
-                        case "1":
-                            exit = true;
-                            break;
-                        case "2":
-                            display.NotImplementedYet();
-                            break;
-                        case "3":
-                            title = display.AskForTitle();
-                            deadline = display.AskForDeadline();
-                            isImportant = display.AskForImportance();
-                            matrix.AddItem(title, deadline, isImportant);
-                            break;
-                        case "4":
-                            quarter = matrix.GetQuarter(display.AskForStatus());
-                            quarter.GetItem(display.AskForIndex()).Mark();
-                            break;
-                        case "5":
-                            quarter = matrix.GetQuarter(display.AskForStatus());
-                            quarter.GetItem(display.AskForIndex()).Unmark();
-                            break;
-                        case "6":
-                            quarter = matrix.GetQuarter(display.AskForStatus());
-                            quarter.RemoveItem(display.AskForIndex());
-                            break;
-                        case "7":
-                            matrix.ArchiveItems();
-                            break;
-                        case "8":
-                            matrix.SaveItemsToFile(FILENAME);
-                            break;
-                        case "9":
-                            matrix.AddItemsFromFile(FILENAME);
-                            break;
-                        case "0":
-                            exit = true;
-                            matrix.ArchiveItems();
-                            matrix.SaveItemsToFile(FILENAME);
-                            break;
-                    }
+                switch (menuInput)
+                {
+                    case "1":
+                        exit = true;
+                        break;
+                    case "2":
+                        display.NotImplementedYet();
+                        break;
+                    case "3":
+                        var title = display.AskForTitle();
+                        var deadline = display.AskForDeadline();
+                        var isImportant = display.AskForImportance();
+                        matrix.AddItem(title, deadline, isImportant);
+                        break;
+                    case "4":
+                        quarter = matrix.GetQuarter(display.AskForStatus());
+                        quarter.GetItem(display.AskForIndex()).Mark();
+                        break;
+                    case "5":
+                        quarter = matrix.GetQuarter(display.AskForStatus());
+                        quarter.GetItem(display.AskForIndex()).Unmark();
+                        break;
+                    case "6":
+                        quarter = matrix.GetQuarter(display.AskForStatus());
+                        quarter.RemoveItem(display.AskForIndex());
+                        break;
+                    case "7":
+                        matrix.ArchiveItems();
+                        break;
+                    case "8":
+                        matrix.SaveItemsToFile(FILENAME);
+                        break;
+                    case "9":
+                        matrix.AddItemsFromFile(FILENAME);
+                        break;
+                    case "0":
+                        exit = true;
+                        matrix.ArchiveItems();
+                        matrix.SaveItemsToFile(FILENAME);
+                        break;
                 }
             }
-
         }
     }
 }
