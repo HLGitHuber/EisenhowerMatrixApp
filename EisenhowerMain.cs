0b966ae9670c01b9d3f5eab998dc6bf654c39112
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-05-06 14:29:53 +0200

    before demo commit

--- EisenhowerMain.cs
+++ EisenhowerMain.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EisenhowerMain
 {
     public class EisenhowerMain
     {  
-        static public void Main(String[] args) 
-        { 
-            Console.WriteLine("Hello Codecool!"); 
+        public static void Main(String[] args)
+        {
+            //TODO make exceptions for inputs
+            
+            var FILENAME = "list";
+            var matrix = new TodoMatrix();
+            var display = new Display();
+            
+            Logic();
+
+            void Logic()
+            {
+                bool exit = false;
+                while (!exit)
+                {
+                    Console.WriteLine(matrix.ToString());
+                    display.ShowMenu();
+                    
+                    string menuInput = Console.ReadLine();
+                    TodoQuarter quarter;
+                    string title;
+                    DateTime deadline;
+                    bool isImportant;
+
+                    switch (menuInput)
+                    {
+                        case "1":
+                            exit = true;
+                            break;
+                        case "2":
+                            display.NotImplementedYet();
+                            break;
+                        case "3":
+                            title = display.AskForTitle();
+                            deadline = display.AskForDeadline();
+                            isImportant = display.AskForImportance();
+                            matrix.AddItem(title, deadline, isImportant);
+                            break;
+                        case "4":
+                            quarter = matrix.GetQuarter(display.AskForStatus());
+                            quarter.GetItem(display.AskForIndex()).Mark();
+                            break;
+                        case "5":
+                            quarter = matrix.GetQuarter(display.AskForStatus());
+                            quarter.GetItem(display.AskForIndex()).Unmark();
+                            break;
+                        case "6":
+                            quarter = matrix.GetQuarter(display.AskForStatus());
+                            quarter.RemoveItem(display.AskForIndex());
+                            break;
+                        case "7":
+                            matrix.ArchiveItems();
+                            break;
+                        case "8":
+                            matrix.SaveItemsToFile(FILENAME);
+                            break;
+                        case "9":
+                            matrix.AddItemsFromFile(FILENAME);
+                            break;
+                        case "0":
+                            exit = true;
+                            matrix.ArchiveItems();
+                            matrix.SaveItemsToFile(FILENAME);
+                            break;
+                    }
+                }
+            }
+
         }
     }
 }
--- TodoMatrix.cs
+++ TodoMatrix.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 
 namespace EisenhowerMain
 {
-    public class TodoMatrix {
+    public class TodoMatrix
+    {
         private Dictionary<string, TodoQuarter> TodoQuarters { get; set; }
 
         public TodoMatrix()
@@ -32,17 +34,17 @@ namespace EisenhowerMain
         public void AddItem(string title, DateTime deadline, bool isImportant = false)
         {
             bool isUrgent = deadline.AddDays(3) <= DateTime.Now;
-            
+
             if (isImportant && isUrgent) TodoQuarters["IU"].AddItem(title, deadline);
             if (isImportant && !isUrgent) TodoQuarters["IN"].AddItem(title, deadline);
             if (!isImportant && isUrgent) TodoQuarters["NU"].AddItem(title, deadline);
             if (!isImportant && !isUrgent) TodoQuarters["NN"].AddItem(title, deadline);
         }
-        
+
         public void AddItemsFromFile(string fileName)
         {
-            
-            var lines = ReadFile(fileName);
+
+            var lines = ReadFile(fileName + ".csv");
             foreach (var line in lines)
             {
                 string[] columns = line.Split(',');
@@ -51,6 +53,45 @@ namespace EisenhowerMain
                 var isImportant = CheckIfIsImportant(columns[2]);
                 AddItem(title, deadline, isImportant);
             }
+        }
+
+        public void SaveItemsToFile(string fileName)
+        {
+            using (var writer = new StreamWriter(fileName + ".csv", false, Encoding.UTF8))
+            {
+                foreach (var quarter in TodoQuarters)
+                {
+                    foreach (var item in quarter.Value.GetItems())
+                    {
+                        bool isImportant = quarter.Key == "IU" || quarter.Key == "IN";
+                        var isImportantString = GetIsImportantString(isImportant);
+                        writer.WriteLine("{0}|{1:dd-MM}|{2}", item.GetTitle(), item.GetDeadline(), isImportantString);
+                    }
+                }
+            }
+        }
+
+        public void ArchiveItems()
+        {
+            foreach (var quarter in TodoQuarters.Values)
+            {
+                quarter.GetItems().RemoveAll(item => item.IsDone);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("TODO:");
+            sb.AppendLine("Important and Urgent:");
+            sb.AppendLine(TodoQuarters["IU"].ToString());
+            sb.AppendLine("Important but Not Urgent:");
+            sb.AppendLine(TodoQuarters["IN"].ToString());
+            sb.AppendLine("Urgent but Not Important:");
+            sb.AppendLine(TodoQuarters["UN"].ToString());
+            sb.AppendLine("Not Important and Not Urgent:");
+            sb.AppendLine(TodoQuarters["NN"].ToString());
+            return sb.ToString();
         }
 
         private string[] ReadFile(string fileName)
@@ -69,43 +110,10 @@ namespace EisenhowerMain
         {
             return isImportant != "" && isImportant != " ";
         }
-    }
-    public void SaveItemsToFile(string fileName)
-    {
-        using (var writer = new StreamWriter(fileName + ".csv", false, Encoding.UTF8))
+        
+        private string GetIsImportantString(bool isImportant)
         {
-            foreach (var quarter in Quarters)
-            { 
-                foreach (var item in quarter.TodoItems)
-                {                  
-                    writer.WriteLine("{0}|{1:dd-MM}|{2}", item.Title, item.Date, item.IsImportant ? "is it important ? " : "");
-                }
-            }
+            return isImportant ? "1" : "";
         }
     }
-
-    public void ArchiveItems()
-    {
-        foreach (var quarter in todoQuarters.Values)
-        {
-            quarter.todoItem.RemoveAll(item => item.IsDone);
-        }
-    }
-
-    public override string ToString()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("TODO:");
-        sb.AppendLine("Important and Urgent:");
-        sb.AppendLine(todoQuarters["IU"].ToString());
-        sb.AppendLine("Important but Not Urgent:");
-        sb.AppendLine(todoQuarters["IN"].ToString());
-        sb.AppendLine("Urgent but Not Important:");
-        sb.AppendLine(todoQuarters["UN"].ToString());
-        sb.AppendLine("Not Important and Not Urgent:");
-        sb.AppendLine(todoQuarters["NN"].ToString());
-        return sb.ToString();
-    }
-
-
 }
